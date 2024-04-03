a05a19e77c65b859872a37e7e4a7bb8a9d9c657b
Frisk-12 <126598650+Frisk-12@users.noreply.github.com>
2024-04-04 01:25:44 +0200

    Update search.py

--- search.py
+++ search.py
@@ -176,7 +176,6 @@ class Search:
                                 del self.state["Q_"+result.id]
                                 del self.state["Qc_"+result.id]
                                 if c or question:
-                                    question = ""
                                     system   = f"""Immagina di essere un consulente specializzato in diritto tributario. Immagina che puoi usufruire di un database composto da svariate sentenze, ordinanze, interpelli e documenti legali relativi al ramo tributario. 
 
 Un utente ha una domanda da porti e sulla base del contenuto di uno dei testi presenti nel database tu hai l'opportunità di aiutare l'utente rispondendo alla sua domanda.
@@ -192,3 +191,4 @@ Ecco il testo: """
                                     self.stream.streamOpenAI(doc_id = result.id, system = system, question = question)
                                     st.write("---")  # Add a horizontal line between results
                                     self.telem.update_session({"AskJurix":question})
+                                    question = ""
