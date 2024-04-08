2291b496d9488435bc827457386107739a61c4a7
Frisk-12 <126598650+Frisk-12@users.noreply.github.com>
2024-04-08 20:13:13 +0200

    Update testing_v3.py

--- testing_v3.py
+++ testing_v3.py
@@ -47,6 +47,7 @@ if __name__ == "__main__":
         startSession = StartSession(state, img)
         
         list_tabs, whitespace, css = startSession.tabsHandler()
+        st.info("Ti preghiamo di considerare che si tratta di una demo gratuita che consiste di un dataset di prassi e giurisprudenza tributaria aggiornato a Novembre 2023.")
         #t1,t2,t3 = st.tabs([s.center(whitespace,"\u2001") for s in list_tabs])
         #st.markdown(css, unsafe_allow_html=True)
         #t1 = st.tabs(["Ricerca"])
