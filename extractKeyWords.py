1011841818cb3f5aca8360ea044787760aded018
Frisk-12 <126598650+Frisk-12@users.noreply.github.com>
2024-04-08 20:43:15 +0200

    Update extractKeyWords.py

--- extractKeyWords.py
+++ extractKeyWords.py
@@ -35,32 +35,32 @@ class KeyWordsExtractor:
             List[str]: A list of keywords extracted from the specific text.
         '''
         
-        #try:
-        # Remove punctuation except alphanumeric characters and spaces
-        self.words = []
-        text = re.sub(r'[^\w\s]', '', text)
-        
-        # Tokenize the text into words
-        words_ = word_tokenize(text.lower())
+        try:
+            # Remove punctuation except alphanumeric characters and spaces
+            self.words = []
+            text = re.sub(r'[^\w\s]', '', text)
+            
+            # Tokenize the text into words
+            words_ = word_tokenize(text.lower())
+    
+            added_words = set()  # Store unique stemmed words
+            
+            for word in words_:
+                if word.lower() not in self.stop_words:
+                    stemmed_word = self.stemmer.stem(word)
+                    if (
+                        stemmed_word not in added_words
+                        and stemmed_word not in {".", ",", "'", '-', '``', ':', "''", ";", "(", ")", "=", "&", "@", "§",
+                                                 "!", "?", "^", '""', '"', '……', '_'}
+                        and not stemmed_word.startswith(".")
+                        and not stemmed_word.endswith(".")
+                        and not (len(stemmed_word) == 1 and stemmed_word.isalpha())
+                    ):
+                        self.words.append(stemmed_word)
+                        added_words.add(stemmed_word)
+            return self.words
 
-        added_words = set()  # Store unique stemmed words
-        
-        for word in words_:
-            if word.lower() not in self.stop_words:
-                stemmed_word = self.stemmer.stem(word)
-                if (
-                    stemmed_word not in added_words
-                    and stemmed_word not in {".", ",", "'", '-', '``', ':', "''", ";", "(", ")", "=", "&", "@", "§",
-                                             "!", "?", "^", '""', '"', '……', '_'}
-                    and not stemmed_word.startswith(".")
-                    and not stemmed_word.endswith(".")
-                    and not (len(stemmed_word) == 1 and stemmed_word.isalpha())
-                ):
-                    self.words.append(stemmed_word)
-                    added_words.add(stemmed_word)
-        return self.words
-
-        #except Exception as e:
-        #    # In caso di errore, stampa il messaggio di errore e gestiscilo opportunamente
-        #    print(f"An error occurred: {str(e)}")
-        #    return []  # Ritorna una lista vuota se si verifica un errore
+        except Exception as e:
+            # In caso di errore, stampa il messaggio di errore e gestiscilo opportunamente
+            print(f"An error occurred: {str(e)}")
+            return []  # Ritorna una lista vuota se si verifica un errore
